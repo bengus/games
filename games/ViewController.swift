99b732630e902643142489580acc04d2fc1b5ff2
Boris Bengus <boris.bengus@borzodelivery.com>
2024-05-15 17:03:20 +0300

    Initial Commit

---
+++ games/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  games
+//
+//  Created by Boris Bengus on 15/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
